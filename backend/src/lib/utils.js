c303bb4814010feebe9d4ee6eb01ea44d313a942
Naveen <nk986960@gmail.com>
2026-03-16 15:22:25 +0530

    fix: cookie secure flag broken on HTTP EB URL, made changes to work on http and https both using env variable.

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,8 +7,8 @@ export const generateToken = (userId, res) => {
     res.cookie('jwt', token, {
         maxAge: 5 * 24 * 60 * 60 * 1000, //5days in miliseconds
         httpOnly: true, //prevent XSS attack, token doesn't gets accessible using "script".
-        sameSite: "strict",
-        secure: process.env.NODE_ENV!=='development'
+        sameSite: "lax",
+        secure: process.env.HTTPS === 'true' //on production(custom domain + SSL), just set env HTTPS=true then cookie will be sent over HTTPS.
     })
 
     return token;
